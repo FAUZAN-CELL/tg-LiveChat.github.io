6daea901b338d7b5f1165b1b597b118bb159f587
tg-LiveChat <110121793+tg-LiveChat@users.noreply.github.com>
2022-07-30 01:23:34 +0700

    Add files via upload

--- src/widget/default-configuration.js
+++ src/widget/default-configuration.js
@@ -1,7 +1,7 @@
 
 export const defaultConfiguration = {
     titleClosed: 'Live Chat!',
-    titleOpen: '@tgLiveChat',
+    titleOpen: 'Telegram',
     closedStyle: 'chat', // button or chat
     closedChatAvatarUrl: '', // only used if closedStyle is set to 'chat'
     cookieExpiration: 1, // in days. Once opened, closed chat title will be shown as button (when closedStyle is set to 'chat')
